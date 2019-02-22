a9352523d8d6dffb8fb37be896401b77280eb139
VJ2053 <VJ2053@UPL60002525.sdmn01.sirius.infra.com>
2019-02-22 08:45:25 +0100

    Initial commit for StarWars samples

---
+++ graphql-maven-plugin-samples/graphql-maven-plugin-samples-StarWars-server/src/main/java/org/graphql/maven/plugin/samples/StarWars/server/App.java
@@ -0,0 +1,13 @@
+package org.graphql.maven.plugin.samples.StarWars.server;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
