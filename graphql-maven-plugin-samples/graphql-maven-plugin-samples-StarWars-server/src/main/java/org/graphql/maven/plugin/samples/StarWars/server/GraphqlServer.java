28b6e6d392218af9436912370b369df03f98f6f1
EtienneSF <etienne_sf@users.sf.net>
2019-03-13 13:54:10 +0100

    A Commit "as is", before renaming the project

--- graphql-maven-plugin-samples/graphql-maven-plugin-samples-StarWars-server/src/main/java/org/graphql/maven/plugin/samples/StarWars/server/GraphqlServer.java
+++ graphql-maven-plugin-samples/graphql-maven-plugin-samples-StarWars-server/src/main/java/org/graphql/maven/plugin/samples/StarWars/server/GraphqlServer.java
@@ -8,8 +8,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *
  */
 @SpringBootApplication
-public class App {
+public class GraphqlServer {
+
 	public static void main(String[] args) {
-		SpringApplication.run(App.class, args);
+		SpringApplication.run(GraphqlServer.class, args);
 	}
+
 }
