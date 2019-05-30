4ad14da93849e5178eba2e411601c1d00a99d8e5
EtienneSF <etienne_sf@users.sf.net>
2019-05-30 15:29:20 +0200

    Getting of dbunits (doesn't work with UUID)

--- graphql-maven-plugin-samples/graphql-maven-plugin-samples-StarWars-server/src/main/java/org/graphql/maven/plugin/samples/server/DatabaseInitializer.java
+++ graphql-maven-plugin-samples/graphql-maven-plugin-samples-StarWars-server/src/main/java/org/graphql/maven/plugin/samples/server/DatabaseInitializer.java
@@ -9,15 +9,11 @@ import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.StandardCopyOption;
+import java.sql.Connection;
+import java.sql.DriverManager;
 
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.dbunit.IDatabaseTester;
-import org.dbunit.JdbcDatabaseTester;
-import org.dbunit.database.DatabaseConfig;
-import org.dbunit.database.IDatabaseConnection;
-import org.dbunit.dataset.csv.CsvDataSet;
-import org.dbunit.operation.DatabaseOperation;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.event.EventListener;
 import org.springframework.core.io.ClassPathResource;
@@ -29,7 +25,7 @@ import org.springframework.stereotype.Component;
  * @author EtienneSF
  */
 @Component
-public class DatabaseTools {
+public class DatabaseInitializer {
 
 	/** The logger for this class */
 	Logger logger = LogManager.getLogger();
@@ -49,38 +45,19 @@ public class DatabaseTools {
 	@EventListener(ApplicationReadyEvent.class)
 	public void initDatabase() throws Exception {
 		try {
-			IDatabaseTester databaseTester = new JdbcDatabaseTester(driverClassName, url, username, password);
-			IDatabaseConnection iConn = databaseTester.getConnection();
-
-			// Important: auto-commit is necessary to keep the dbsetup change in the database
-			boolean autoCommitBefore = databaseTester.getConnection().getConnection().getAutoCommit();
-			iConn.getConnection().setAutoCommit(true);
-			iConn.getConfig().setProperty(DatabaseConfig.FEATURE_ALLOW_EMPTY_FIELDS, Boolean.TRUE);
+			Class.forName("org.h2.Driver");
+			Connection connection = DriverManager.getConnection("jdbc:h2:mem:testdb", "sa", "");
+			connection.setAutoCommit(true);
 
 			// Let's load our data
-			// URL url = new ClassPathResource("starwars_data").getURL();
-			// databaseTester.setDataSet(new CsvURLDataSet(url));
-			Path dir = Files.createTempDirectory(FILE_PREFIX);
-			createTempFileForCSV(dir, "droid.csv");
-			createTempFileForCSV(dir, "human.csv");
-			createTempFileForCSV(dir, "episode.csv");
-			createTempFileForCSV(dir, "character.csv");
-			createTempFileForCSV(dir, "character_appears_in.csv");
-			createTempFileForCSV(dir, "droid_appears_in.csv");
-			createTempFileForCSV(dir, "human_appears_in.csv");
-			createTempFileForCSV(dir, "character_friends.csv");
-			createTempFileForCSV(dir, "droid_friends.csv");
-			createTempFileForCSV(dir, "table-ordering.txt");
-			// IDataSet dataSets[] = { new CsvDataSet(createTempFileForCSV(dir, "data/droid.csv", "droid")),
-			// new CsvDataSet(createTempFileForCSV(dir, "data/human.csv", "human")), };
-			// databaseTester.setDataSet(new CsvDataSet(dir.toFile()));
+			CSVLoader loader = new CSVLoader(connection, "data");
+			loader.loadCSV("droid", false);
+			loader.loadCSV("human", false);
+			loader.loadCSV("episode", false);
+			loader.loadCSV("droid_appears_in", false);
+			loader.loadCSV("human_appears_in", false);
+			loader.loadCSV("character_friends", false);
 
-			// will call default setUpOperation
-			// databaseTester.onSetup();
-			DatabaseOperation.CLEAN_INSERT.execute(iConn, new CsvDataSet(dir.toFile()));
-
-			// and we restore the autoCommit status before leaving
-			databaseTester.getConnection().getConnection().setAutoCommit(autoCommitBefore);
 		} catch (Exception e) {
 			// An error occured. We logged, but don't block the start of the server : very often, the issue is a gap
 			// with the database model. And we need the in-memory database to be started, to check that.
