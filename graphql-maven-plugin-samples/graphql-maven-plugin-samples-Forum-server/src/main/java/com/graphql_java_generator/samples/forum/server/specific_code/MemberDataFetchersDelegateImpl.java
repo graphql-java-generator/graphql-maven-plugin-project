f0860390c8c22fa3c97e149cf7324200d2df32d9
EtienneSF <etienne_sf@users.sf.net>
2019-12-05 13:25:00 +0100

    Sample code removed (was a test before the code generation)

---
+++ graphql-maven-plugin-samples/graphql-maven-plugin-samples-Forum-server/src/main/java/com/graphql_java_generator/samples/forum/server/specific_code/MemberDataFetchersDelegateImpl.java
@@ -0,0 +1,31 @@
+package com.graphql_java_generator.samples.forum.server.specific_code;
+
+import java.util.List;
+import java.util.UUID;
+
+import javax.annotation.Resource;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.stereotype.Component;
+
+import com.graphql_java_generator.samples.forum.server.Member;
+import com.graphql_java_generator.samples.forum.server.MemberDataFetchersDelegate;
+import com.graphql_java_generator.samples.forum.server.jpa.MemberRepository;
+
+@Component
+public class MemberDataFetchersDelegateImpl implements MemberDataFetchersDelegate {
+
+	/** The logger for this instance */
+	protected Logger logger = LogManager.getLogger();
+
+	@Resource
+	MemberRepository memberRepository;
+
+	@Override
+	public List<Member> batchLoader(List<UUID> keys) {
+		logger.debug("Batch loading {} members", keys.size());
+		return memberRepository.findByIds(keys);
+	}
+
+}
