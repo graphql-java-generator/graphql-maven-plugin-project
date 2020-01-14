4a6b05bd4431fdd2cf3a0ab3e7c334758c994201
EtienneSF <etienne_sf@users.sf.net>
2020-01-14 23:42:55 +0100

    Added a missing file in previous commit

---
+++ graphql-maven-plugin-samples/graphql-maven-plugin-samples-allGraphQLCases-server/src/main/java/org/allGraphQLCases/server/impl/DataFetchersDelegateAllFieldCasesImpl.java
@@ -0,0 +1,107 @@
+/**
+ * 
+ */
+package org.allGraphQLCases.server.impl;
+
+import java.util.List;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+
+import javax.annotation.Resource;
+
+import org.allGraphQLCases.server.AllFieldCases;
+import org.allGraphQLCases.server.AllFieldCasesWithIdSubtype;
+import org.allGraphQLCases.server.AllFieldCasesWithoutIdSubtype;
+import org.allGraphQLCases.server.DataFetchersDelegateAllFieldCases;
+import org.allGraphQLCases.server.Human;
+import org.dataloader.DataLoader;
+import org.springframework.stereotype.Component;
+
+import graphql.schema.DataFetchingEnvironment;
+
+/**
+ * @author EtienneSF
+ *
+ */
+@Component
+public class DataFetchersDelegateAllFieldCasesImpl implements DataFetchersDelegateAllFieldCases {
+
+	@Resource
+	DataGenerator generator;
+
+	@Override
+	public List<String> comments(DataFetchingEnvironment dataFetchingEnvironment, AllFieldCases source) {
+		return generator.generateInstanceList(String.class, 10);
+	}
+
+	@Override
+	public List<Boolean> booleans(DataFetchingEnvironment dataFetchingEnvironment, AllFieldCases source) {
+		return generator.generateInstanceList(Boolean.class, 10);
+	}
+
+	@Override
+	public List<String> aliases(DataFetchingEnvironment dataFetchingEnvironment, AllFieldCases source) {
+		return generator.generateInstanceList(String.class, 10);
+	}
+
+	@Override
+	public List<String> planets(DataFetchingEnvironment dataFetchingEnvironment, AllFieldCases source) {
+		return generator.generateInstanceList(String.class, 10);
+	}
+
+	@Override
+	public List<Human> friends(DataFetchingEnvironment dataFetchingEnvironment, AllFieldCases source) {
+		return generator.generateInstanceList(Human.class, 10);
+	}
+
+	@Override
+	public List<AllFieldCases> batchLoader(List<UUID> keys) {
+		return generator.generateInstanceList(AllFieldCases.class, keys.size());
+	}
+
+	@Override
+	public CompletableFuture<AllFieldCasesWithIdSubtype> oneWithIdSubType(
+			DataFetchingEnvironment dataFetchingEnvironment, DataLoader<UUID, AllFieldCasesWithIdSubtype> dataLoader,
+			AllFieldCases source) {
+		return dataLoader.load(UUID.randomUUID());
+	}
+
+	@Override
+	public List<AllFieldCasesWithIdSubtype> listWithIdSubTypes(DataFetchingEnvironment dataFetchingEnvironment,
+			AllFieldCases source, Integer nbItems, Boolean uppercaseName, String textToAppendToTheForname) {
+		List<AllFieldCasesWithIdSubtype> list = generator.generateInstanceList(AllFieldCasesWithIdSubtype.class,
+				nbItems);
+
+		for (AllFieldCasesWithIdSubtype item : list) {
+			if (uppercaseName != null && uppercaseName) {
+				item.setName(item.getName().toUpperCase());
+			}
+			item.setName(item.getName() + textToAppendToTheForname);
+		}
+
+		return list;
+	}
+
+	@Override
+	public AllFieldCasesWithoutIdSubtype oneWithoutIdSubType(DataFetchingEnvironment dataFetchingEnvironment,
+			AllFieldCases source) {
+		return source.getOneWithoutIdSubType();
+	}
+
+	@Override
+	public List<AllFieldCasesWithoutIdSubtype> listWithoutIdSubTypes(DataFetchingEnvironment dataFetchingEnvironment,
+			AllFieldCases source, Integer nbItems, Boolean uppercaseName, String textToAppendToTheForname) {
+		List<AllFieldCasesWithoutIdSubtype> list = generator.generateInstanceList(AllFieldCasesWithoutIdSubtype.class,
+				nbItems);
+
+		for (AllFieldCasesWithoutIdSubtype item : list) {
+			if (uppercaseName != null && uppercaseName) {
+				item.setName(item.getName().toUpperCase());
+			}
+			item.setName(item.getName() + textToAppendToTheForname);
+		}
+
+		return list;
+	}
+
+}
