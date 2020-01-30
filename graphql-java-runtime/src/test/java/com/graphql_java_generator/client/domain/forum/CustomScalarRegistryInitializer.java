db987e5d2f4a4bc30cb4033c6900e8383e5cb534
EtienneSF <etienne_sf@users.sf.net>
2020-01-30 09:01:38 +0100

    junit test corrected (adapted for custom scalars)

--- graphql-java-runtime/src/test/java/com/graphql_java_generator/client/domain/forum/CustomScalarRegistryInitializer.java
+++ graphql-java-runtime/src/test/java/com/graphql_java_generator/client/domain/forum/CustomScalarRegistryInitializer.java
@@ -1,12 +1,19 @@
 package com.graphql_java_generator.client.domain.forum;
 
+import com.graphql_java_generator.CustomScalarRegistry;
 import com.graphql_java_generator.CustomScalarRegistryImpl;
 import com.graphql_java_generator.customcalars.GraphQLScalarTypeDate;
 
 public class CustomScalarRegistryInitializer {
 
+	/**
+	 * Initialization of the {@link CustomScalarRegistry} with all known custom scalars, that is with all custom scalars
+	 * defined in the project pom
+	 */
 	public void initCustomScalarRegistry() {
-		CustomScalarRegistryImpl.customScalarRegistry.registerGraphQLScalarType(new GraphQLScalarTypeDate());
+		CustomScalarRegistry customScalarRegistry = new CustomScalarRegistryImpl();
+		customScalarRegistry.registerGraphQLScalarType(new GraphQLScalarTypeDate());
+		CustomScalarRegistryImpl.customScalarRegistry = customScalarRegistry;
 	}
 
 }
