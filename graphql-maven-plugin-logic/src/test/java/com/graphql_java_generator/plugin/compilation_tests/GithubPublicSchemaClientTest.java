7097482a342b6db9b8efd83ce0173a93a5939714
etienne-sf <etienne_sf@users.sf.net>
2020-04-12 17:36:41 +0200

    Shopify and Github test are disabled (too long to run each time)

--- graphql-maven-plugin-logic/src/test/java/com/graphql_java_generator/plugin/compilation_tests/GithubPublicSchemaClientTest.java
+++ graphql-maven-plugin-logic/src/test/java/com/graphql_java_generator/plugin/compilation_tests/GithubPublicSchemaClientTest.java
@@ -1,6 +1,7 @@
 package com.graphql_java_generator.plugin.compilation_tests;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.test.context.ContextConfiguration;
@@ -10,7 +11,7 @@ import com.graphql_java_generator.plugin.test.helper.PluginConfigurationTestHelp
 
 import graphql.mavenplugin_notscannedbyspring.Github_Client_SpringConfiguration;
 
-//@Disabled
+@Disabled // Too long to run each time
 @ExtendWith(SpringExtension.class)
 @ContextConfiguration(classes = { Github_Client_SpringConfiguration.class })
 @Tag("github")
