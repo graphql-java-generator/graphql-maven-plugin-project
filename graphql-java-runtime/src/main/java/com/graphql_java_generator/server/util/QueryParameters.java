e96d98b159be063e3874d6e4a31fb7ccf0b55e3c
etienne-sf <etienne_sf@users.sf.net>
2020-10-25 17:24:40 +0100

    Removed a compilation warning

--- graphql-java-runtime/src/main/java/com/graphql_java_generator/server/util/QueryParameters.java
+++ graphql-java-runtime/src/main/java/com/graphql_java_generator/server/util/QueryParameters.java
@@ -5,9 +5,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 /**
- * Graphql clients can send GET or POST HTTP requests.  The spec does not make an explicit
- * distinction.  So you may need to handle both.  The following was tested using
- * a graphiql client tool found here : https://github.com/skevy/graphiql-app
+ * Graphql clients can send GET or POST HTTP requests. The spec does not make an explicit distinction. So you may need
+ * to handle both. The following was tested using a graphiql client tool found here :
+ * https://github.com/skevy/graphiql-app
  *
  * You should consider bundling graphiql in your application
  *
@@ -19,40 +19,39 @@ import java.util.Map;
  */
 public class QueryParameters {
 
-    private String query;
-    private String operationName;
-    private Map<String, Object> variables = Collections.emptyMap();
+	private String query;
+	private String operationName;
+	private Map<String, Object> variables = Collections.emptyMap();
 
-    public String getQuery() {
-        return query;
-    }
+	public String getQuery() {
+		return query;
+	}
 
-    public String getOperationName() {
-        return operationName;
-    }
+	public String getOperationName() {
+		return operationName;
+	}
 
-    public Map<String, Object> getVariables() {
-        return variables;
-    }
+	public Map<String, Object> getVariables() {
+		return variables;
+	}
 
-    public static QueryParameters from(String queryMessage) {
-        QueryParameters parameters = new QueryParameters();
-        Map<String, Object> json = JsonKit.toMap(queryMessage);
-        parameters.query = (String) json.get("query");
-        parameters.operationName = (String) json.get("operationName");
-        parameters.variables = getVariables(json.get("variables"));
-        return parameters;
-    }
+	public static QueryParameters from(String queryMessage) {
+		QueryParameters parameters = new QueryParameters();
+		Map<String, Object> json = JsonKit.toMap(queryMessage);
+		parameters.query = (String) json.get("query");
+		parameters.operationName = (String) json.get("operationName");
+		parameters.variables = getVariables(json.get("variables"));
+		return parameters;
+	}
 
-
-    private static Map<String, Object> getVariables(Object variables) {
-        if (variables instanceof Map) {
-            Map<?, ?> inputVars = (Map) variables;
-            Map<String, Object> vars = new HashMap<>();
-            inputVars.forEach((k, v) -> vars.put(String.valueOf(k), v));
-            return vars;
-        }
-        return JsonKit.toMap(String.valueOf(variables));
-    }
+	private static Map<String, Object> getVariables(Object variables) {
+		if (variables instanceof Map) {
+			Map<?, ?> inputVars = (Map<?, ?>) variables;
+			Map<String, Object> vars = new HashMap<>();
+			inputVars.forEach((k, v) -> vars.put(String.valueOf(k), v));
+			return vars;
+		}
+		return JsonKit.toMap(String.valueOf(variables));
+	}
 
 }
