1a9c936af4f66edda6ae8e07e6b8a78923d7bbdf
etienne-sf <etienne_sf@users.sf.net>
2021-01-03 19:13:36 +0100

    Last try, before using Marcos Barbero sample

---
+++ graphql-maven-plugin-samples/graphql-maven-plugin-samples-OAuth-authorization-server/src/main/java/org/boot_doc/JwkSetEndpoint.java
@@ -0,0 +1,30 @@
+package org.boot_doc;
+
+import java.security.KeyPair;
+import java.security.Principal;
+import java.security.interfaces.RSAPublicKey;
+import java.util.Map;
+
+import org.springframework.security.oauth2.provider.endpoint.FrameworkEndpoint;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.nimbusds.jose.jwk.JWKSet;
+import com.nimbusds.jose.jwk.RSAKey;
+
+@FrameworkEndpoint
+class JwkSetEndpoint {
+	KeyPair keyPair;
+
+	public JwkSetEndpoint(KeyPair keyPair) {
+		this.keyPair = keyPair;
+	}
+
+	@GetMapping("/.well-known/jwks.json")
+	@ResponseBody
+	public Map<String, Object> getKey(Principal principal) {
+		RSAPublicKey publicKey = (RSAPublicKey) this.keyPair.getPublic();
+		RSAKey key = new RSAKey.Builder(publicKey).build();
+		return new JWKSet(key).toJSONObject();
+	}
+}
--- graphql-maven-plugin-samples/graphql-maven-plugin-samples-OAuth-authorization-server/src/main/java/org/boot_doc/SimpleAuthorizationServerApplication.java
+++ graphql-maven-plugin-samples/graphql-maven-plugin-samples-OAuth-authorization-server/src/main/java/org/boot_doc/SimpleAuthorizationServerApplication.java
@@ -3,9 +3,13 @@
  */
 package org.boot_doc;
 
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.NoSuchAlgorithmException;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.context.annotation.Bean;
 
 /**
  * <PRE>
@@ -13,14 +17,23 @@ curl clientid:clientpwd@localhost:8181/oauth/token -dgrant_type=client_credentia
  * </PRE>
  * 
  * @author etienne-sf
+ * @see https://aaronparecki.com/oauth-2-simplified
  * @see https://docs.spring.io/spring-security-oauth2-boot/docs/current/reference/html5/
  */
-@EnableAuthorizationServer
+// @EnableAuthorizationServer
 @SpringBootApplication
 public class SimpleAuthorizationServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SimpleAuthorizationServerApplication.class, args);
+	}
+
+	@Bean
+	public KeyPair keyPairBean() throws NoSuchAlgorithmException {
+		KeyPairGenerator gen = KeyPairGenerator.getInstance("RSA");
+		gen.initialize(2048);
+		KeyPair keyPair = gen.generateKeyPair();
+		return keyPair;
 	}
 
 }
