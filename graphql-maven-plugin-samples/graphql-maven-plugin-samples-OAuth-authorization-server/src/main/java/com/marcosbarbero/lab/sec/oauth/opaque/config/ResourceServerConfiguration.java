1f286e4e13923f2a8cb05d6d0631a5e11fcd01c5
etienne-sf <etienne_sf@users.sf.net>
2021-01-09 16:04:20 +0100

    Add a Hello World URL, for the maven build to check the server start

--- graphql-maven-plugin-samples/graphql-maven-plugin-samples-OAuth-authorization-server/src/main/java/com/marcosbarbero/lab/sec/oauth/opaque/config/ResourceServerConfiguration.java
+++ graphql-maven-plugin-samples/graphql-maven-plugin-samples-OAuth-authorization-server/src/main/java/com/marcosbarbero/lab/sec/oauth/opaque/config/ResourceServerConfiguration.java
@@ -1,6 +1,7 @@
 package com.marcosbarbero.lab.sec.oauth.opaque.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
@@ -10,15 +11,22 @@ import org.springframework.security.oauth2.provider.token.TokenStore;
 @EnableResourceServer
 public class ResourceServerConfiguration extends ResourceServerConfigurerAdapter {
 
-    private final TokenStore tokenStore;
+	private final TokenStore tokenStore;
 
-    public ResourceServerConfiguration(final TokenStore tokenStore) {
-        this.tokenStore = tokenStore;
-    }
+	public ResourceServerConfiguration(final TokenStore tokenStore) {
+		this.tokenStore = tokenStore;
+	}
 
-    @Override
-    public void configure(final ResourceServerSecurityConfigurer resources) {
-        resources.tokenStore(tokenStore);
-    }
+	@Override
+	public void configure(final ResourceServerSecurityConfigurer resources) {
+		resources.tokenStore(tokenStore);
+	}
+
+	@Override
+	public void configure(HttpSecurity http) throws Exception {
+		// We need to let an open access to this Hello World URL, so that the maven build can check when the OAuth
+		// authorization server is properly started
+		http.authorizeRequests(authz -> authz.antMatchers("/helloworld.html").permitAll());
+	}
 
 }
--- graphql-maven-plugin-samples/graphql-maven-plugin-samples-OAuth-authorization-server/src/main/java/com/marcosbarbero/lab/sec/oauth/opaque/config/WebSecurityConfiguration.java
+++ graphql-maven-plugin-samples/graphql-maven-plugin-samples-OAuth-authorization-server/src/main/java/com/marcosbarbero/lab/sec/oauth/opaque/config/WebSecurityConfiguration.java
@@ -1,5 +1,7 @@
 package com.marcosbarbero.lab.sec.oauth.opaque.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -10,48 +12,45 @@ import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-import javax.sql.DataSource;
-
 @EnableWebSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
-    private final DataSource dataSource;
+	private final DataSource dataSource;
 
-    private PasswordEncoder passwordEncoder;
-    private UserDetailsService userDetailsService;
+	private PasswordEncoder passwordEncoder;
+	private UserDetailsService userDetailsService;
 
-    public WebSecurityConfiguration(final DataSource dataSource) {
-        this.dataSource = dataSource;
-    }
+	public WebSecurityConfiguration(final DataSource dataSource) {
+		this.dataSource = dataSource;
+	}
 
-    @Override
-    protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
-        auth.userDetailsService(userDetailsService())
-                .passwordEncoder(passwordEncoder());
-    }
+	@Override
+	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
+		auth.userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder());
+	}
 
-    @Bean
-    @Override
-    public AuthenticationManager authenticationManagerBean() throws Exception {
-        return super.authenticationManagerBean();
-    }
+	@Bean
+	@Override
+	public AuthenticationManager authenticationManagerBean() throws Exception {
+		return super.authenticationManagerBean();
+	}
 
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        if (passwordEncoder == null) {
-            passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
-        }
-        return passwordEncoder;
-    }
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		if (passwordEncoder == null) {
+			passwordEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		}
+		return passwordEncoder;
+	}
 
-    @Bean
-    @Override
-    public UserDetailsService userDetailsService() {
-        if (userDetailsService == null) {
-            userDetailsService = new JdbcDaoImpl();
-            ((JdbcDaoImpl) userDetailsService).setDataSource(dataSource);
-        }
-        return userDetailsService;
-    }
+	@Bean
+	@Override
+	public UserDetailsService userDetailsService() {
+		if (userDetailsService == null) {
+			userDetailsService = new JdbcDaoImpl();
+			((JdbcDaoImpl) userDetailsService).setDataSource(dataSource);
+		}
+		return userDetailsService;
+	}
 
 }
