fbe47e2958781511847c9c71b71a4a452e02ffa3
etienne-sf <etienne_sf@users.sf.net>
2021-01-12 17:22:20 +0100

    Removed a useless class

--- graphql-maven-plugin-samples/graphql-maven-plugin-samples-allGraphQLCases-server/src/main/java/org/allGraphQLCases/server/oauth2/ResourceServerConfiguration.java
+++
@@ -1,9 +0,0 @@
-package org.allGraphQLCases.server.oauth2;
-
-//@Configuration
-//@EnableResourceServer
-//@EnableWebSecurity
-public class ResourceServerConfiguration // extends ResourceServerConfigurerAdapter
-{
-
-}
