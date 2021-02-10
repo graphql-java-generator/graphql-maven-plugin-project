6921aacabe36fdefc42cc178dcb7d995f812c938
EtienneSF <etienne_sf@users.sf.net>
2021-02-10 19:45:28 +0100

    Checking new site in

---
+++ graphql-maven-plugin-samples/graphql-maven-plugin-samples-OAuth-authorization-server/apidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.marcosbarbero.lab.sec.oauth.opaque"},{"l":"com.marcosbarbero.lab.sec.oauth.opaque.config"},{"l":"com.marcosbarbero.lab.sec.oauth.opaque.web"}]
