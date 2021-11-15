04b358306ebbea37884b37b7942b1346ececaedf
etienne-sf <etienne_sf@users.sf.net>
2021-11-15 08:46:06 +0100

    Issue #105: allow Spring bean overriding

--- graphql-maven-plugin-samples/graphql-maven-plugin-samples-allGraphQLCases-client/src/test/java/org/allGraphQLCases/SpringTestConfig.java
+++ graphql-maven-plugin-samples/graphql-maven-plugin-samples-allGraphQLCases-client/src/test/java/org/allGraphQLCases/SpringTestConfig.java
@@ -1,20 +1,27 @@
 package org.allGraphQLCases;
 
 import org.allGraphQLCases.client.util.MyQueryTypeExecutorAllGraphQLCases;
+import org.allGraphQLCases.client.util.SpringConfigurationAllGraphQLCases;
 import org.allGraphQLCases.client2.util.MyQueryTypeExecutorAllGraphQLCases2;
 import org.allGraphQLCases.demo.impl.PartialDirectQueries;
 import org.forum.client.util.QueryExecutorForum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.security.oauth2.client.registration.ReactiveClientRegistrationRepository;
 import org.springframework.security.oauth2.client.web.reactive.function.client.ServerOAuth2AuthorizedClientExchangeFilterFunction;
 import org.springframework.security.oauth2.client.web.server.UnAuthenticatedServerOAuth2AuthorizedClientRepository;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.graphql_java_generator.client.GraphQLConfiguration;
 import com.graphql_java_generator.client.graphqlrepository.EnableGraphQLRepositories;
+
+import reactor.netty.http.client.HttpClient;
 
 @Configuration
 @SpringBootApplication
@@ -25,6 +32,9 @@ import com.graphql_java_generator.client.graphqlrepository.EnableGraphQLReposito
 		"org.allGraphQLCases.two_graphql_servers" })
 @SuppressWarnings("deprecation")
 public class SpringTestConfig {
+
+	@Autowired
+	SpringConfigurationAllGraphQLCases defaultConfiguration;
 
 	@Bean
 	ServerOAuth2AuthorizedClientExchangeFilterFunction serverOAuth2AuthorizedClientExchangeFilterFunctionAllGraphQLCases(
@@ -39,5 +49,14 @@ public class SpringTestConfig {
 	ServerOAuth2AuthorizedClientExchangeFilterFunction serverOAuth2AuthorizedClientExchangeFilterFunctionForum(
 			ReactiveClientRegistrationRepository clientRegistrations) {
 		return null;
+	}
+
+	@Bean
+	@Primary
+	public WebClient webClientAllGraphQLCases(String graphqlEndpointAllGraphQLCases, //
+			@Autowired(required = false) @Qualifier("httpClientAllGraphQLCases") HttpClient httpClientAllGraphQLCases,
+			@Autowired(required = false) @Qualifier("serverOAuth2AuthorizedClientExchangeFilterFunctionAllGraphQLCases") ServerOAuth2AuthorizedClientExchangeFilterFunction serverOAuth2AuthorizedClientExchangeFilterFunctionAllGraphQLCases) {
+		return defaultConfiguration.webClientAllGraphQLCases(graphqlEndpointAllGraphQLCases, httpClientAllGraphQLCases,
+				serverOAuth2AuthorizedClientExchangeFilterFunctionAllGraphQLCases);
 	}
 }
