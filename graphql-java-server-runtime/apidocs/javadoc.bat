05ea4f8050d1607201852cd8532db6529cdfecaf
EtienneSF <etienne_sf@users.sf.net>
2022-01-30 22:30:04 +0100

    Checking new site in

---
+++ graphql-java-server-runtime/apidocs/javadoc.bat
@@ -0,0 +1,1 @@
+cmd.exe /X /C "C:\APPLIRTE64\appli\java\jdk\bin\javadoc.exe -J-Dhttps.proxyHost=127.0.0.1 -J-Dhttps.proxyPort=3128 -J-Dhttp.nonProxyHosts="localhost^|127.0.0.1^|my_local.lesgauthier.fr^|*.gdfsuez.net^|192.168.56.*" @options @argfile"
