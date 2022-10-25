cbdfedd6732c24dc0faa3ccc5564f7ec91206647
Mikkel Agesen <mikkel.agesen@elsevier.com>
2022-10-25 10:05:35 +0200

    Adds a sub-module for integration that configured prefixes and suffixes are used correctly in code generation.

---
+++ graphql-maven-plugin-samples/graphql-maven-plugin-samples-allGraphQLCases-prefixsuffix/src/test/java/org/allGraphQLCases/client/prefixsuffix/PrefixSuffixClientTest.java
@@ -0,0 +1,28 @@
+package org.allGraphQLCases.client.prefixsuffix;
+
+import org.allGraphQLCases.prefixsuffix.*;
+import org.allGraphQLCases.prefixsuffix.util.MyQueryTypeExecutor;
+import org.allGraphQLCases.prefixsuffix.util.MyQueryTypeResponse;
+import org.allGraphQLCases.prefixsuffix.util.MyQueryTypeRootResponse;
+
+/**
+ * Verifies that that prefixes and suffix are used correctly in client code generation.
+ */
+@SuppressWarnings("unused")
+public class PrefixSuffixClientTest {
+
+	// expect configured prefixes and suffixes to be used on inputs, types, interfaces, unions, and enums
+	private InputPrefixDroidInputInputSuffix droidInput;
+	private TypePrefixDroidTypeSuffix droidType;
+	private InterfacePrefixCharacterInterfaceSuffix characterInterface;
+	private UnionPrefixAnyCharacterUnionSuffix anyCharacterUnion;
+	private EnumPrefixEpisodeEnumSuffix episodeEnum;
+
+	// do not expect type prefix or suffix on classes that already have a suffix
+	private MyQueryTypeExecutor myQueryTypeExecutor;
+	private MyQueryTypeResponse myQueryTypeResponse;
+	private MyQueryTypeRootResponse myQueryTypeRootResponse;
+
+	// No test execution: the simple fact that this class compiles is enough.
+}
+
